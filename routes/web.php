afd7a12c6e1fc11579f2482040936a0ad95c5b7d
Miguel Hernandez <miguel.angel.hj123@gmail.com>
2024-08-31 17:17:11 -0400

    v1.0.2

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,11 @@ Route::get('cursos/{curso}', [cursoController::class, 'show']);
 Route::get('/', homeController::class);
 
 Route::controller(cursoController::class)->group(function(){
-    Route::get('cursos', 'index');
-    Route::get('cursos/create', 'create');
-    Route::get('cursos/{curso}', 'show');
+    Route::get('cursos', 'index')->name('cursos.index');
+    Route::get('cursos/create', 'create')->name('cursos.create');
+    Route::post('cursos', 'store')->name('cursos.store');
+    Route::get('cursos/{curso}', 'show')->name('cursos.show');
+    Route::get('cursos/{curso}/edit', 'edit')->name('cursos.edit');
+    Route::put('cursos/{curso}', 'update')->name('cursos.update');
 });
 
